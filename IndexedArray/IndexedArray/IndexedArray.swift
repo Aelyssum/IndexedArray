9bfb21459523be70881245edcdaa78d3312fcdec
Allan Evans <allan@aelyssum.com>
2016-10-14 09:47:55 -0700

    Summary:  Updated for Swift 3

--- IndexedArray/IndexedArray/IndexedArray.swift
+++ IndexedArray/IndexedArray/IndexedArray.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-class IndexedArray<Element: Hashable>: NSObject, SequenceType {
+class IndexedArray<Element: Hashable>: NSObject, Sequence {
     
-    private var arrayOfElements = [Element]()
-    private var indexOfElements = [Int: Int]()
+    fileprivate var arrayOfElements = [Element]()
+    fileprivate var indexOfElements = [Int: Int]()
     
     var count: Int {
         return arrayOfElements.count
@@ -45,7 +45,7 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         return self.arrayOfElements[index]
     }
     
-    func elementAtIndex<H: Hashable>(index: H) -> Element? {
+    func elementAtIndex<H: Hashable>(_ index: H) -> Element? {
         if let thisIndex = self.indexOfElements[index.hashValue] {
             return self.arrayOfElements[thisIndex]
         } else {
@@ -55,11 +55,11 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
     
     // MARK: SequenceType methods
     
-    func generate() -> AnyGenerator<Element> {
+    func makeIterator() -> AnyIterator<Element> {
         // keep the index of the next element in the iteration
         var nextIndex = 0
         
-        return AnyGenerator<Element> {
+        return AnyIterator<Element> {
             var returnValue: Element?
             if (nextIndex < self.arrayOfElements.count) {
                 returnValue = self.arrayOfElements[nextIndex]
@@ -71,7 +71,7 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
 
     // MARK: Add, modify, and delete methods
     
-    func insert(element: Element) {
+    func insert(_ element: Element) {
         if let thisIndex = self.indexOfElements[element.hashValue] {
             self.arrayOfElements[thisIndex] = element
         } else {
@@ -80,17 +80,17 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         }
     }
     
-    func remove(element: Element) {
+    func remove(_ element: Element) {
         if let thisIndex = self.indexOfElements[element.hashValue] {
-            self.arrayOfElements.removeAtIndex(thisIndex)
+            self.arrayOfElements.remove(at: thisIndex)
             self.indexOfElements[element.hashValue] = nil
             self.buildDictionary()
         }
     }
     
-    func removeAtIndex<H: Hashable>(index: H) {
+    func removeAtIndex<H: Hashable>(_ index: H) {
         if let thisIndex = self.indexOfElements[index.hashValue] {
-            self.arrayOfElements.removeAtIndex(thisIndex)
+            self.arrayOfElements.remove(at: thisIndex)
             self.indexOfElements[index.hashValue] = nil
             self.buildDictionary()
         }
@@ -101,7 +101,7 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         self.indexOfElements.removeAll()
     }
     
-    private func buildDictionary() {
+    fileprivate func buildDictionary() {
         // Build Dictionary can only be called within another task executing on queue
         if !self.arrayOfElements.isEmpty {
             for index in 0...self.arrayOfElements.count-1 {
@@ -114,33 +114,33 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
     
     // MARK:  Functional methods
     
-    func sort(handler: (first: Element, second: Element) -> Bool) -> Self {
-        let returnArray = self.dynamicType.init()
-        returnArray.arrayOfElements = self.arrayOfElements.sort(handler)
+    func sorted(by handler: (_ first: Element, _ second: Element) -> Bool) -> Self {
+        let returnArray = type(of: self).init()
+        returnArray.arrayOfElements = self.arrayOfElements.sorted(by: handler)
         returnArray.buildDictionary()
         return returnArray
     }
     
-    func filter(handler: (element: Element) -> Bool) -> Self {
-        let returnArray = self.dynamicType.init()
+    func filter(_ handler: (_ element: Element) -> Bool) -> Self {
+        let returnArray = type(of: self).init()
         returnArray.arrayOfElements = self.arrayOfElements.filter(handler)
         returnArray.buildDictionary()
         return returnArray
     }
     
-    func map<T>(transform: Element throws -> T) rethrows -> [T] {
+    func map<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
         return try self.arrayOfElements.map(transform)
     }
     
-    func reduce<T>(initial: T?, combine: (T?, Element) -> T?) -> T? {
+    func reduce<T>(_ initial: T?, combine: (T?, Element) -> T?) -> T? {
         var returnVal: T?
-        returnVal = self.arrayOfElements.reduce(initial, combine: combine)
+        returnVal = self.arrayOfElements.reduce(initial, combine)
         return returnVal
     }
     
-    func reduce<T>(initial: T, combine: (T, Element) throws -> T) rethrows -> T {
+    func reduce<T>(_ initial: T, combine: (T, Element) throws -> T) rethrows -> T {
         var returnVal: T!
-        returnVal = try self.arrayOfElements.reduce(initial, combine: combine)
+        returnVal = try self.arrayOfElements.reduce(initial, combine)
         return returnVal
     }
     
