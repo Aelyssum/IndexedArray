b05ecc2efe2680b12b51b1a1434a75c05c099190
Allan Evans <allan@aelyssum.com>
2016-10-14 14:28:08 -0700

    Summary:  Added thread safety with custom dispatch queue.

--- IndexedArray/IndexedArray/IndexedArray.swift
+++ IndexedArray/IndexedArray/IndexedArray.swift
@@ -21,18 +21,27 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         return arrayOfElements.count == 0
     }
     
+    // MARK: Thread safety
+    
+    var queue: dispatch_queue_t
+    
     required override init() {
         super.init()
+        queue = dispatch_queue_create(label: "com.Aelyssum.IndexedArray.\(NSDate())", DISPATCH_QUEUE_CONCURRENT)
     }
     
     // MARK: Accessor methods
     
     var first: Element? {
-        return arrayOfElements.first
+        return queue.dispatch_sync() {
+            return arrayOfElements.first
+        }
     }
     
     var last: Element? {
-        return arrayOfElements.last
+        return queue.dispatch_sync() {
+            return arrayOfElements.last
+        }
     }
 
     subscript(index: Int) -> Element {
@@ -42,14 +51,18 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         assert(index >= 0 && index < self.count,
                "Index \(index) out of range 0...\(self.count-1)"
         )
-        return self.arrayOfElements[index]
+        return queue.dispatch_sync() {
+            return self.arrayOfElements[index]
+        }
     }
     
     func elementAtIndex<H: Hashable>(index: H) -> Element? {
-        if let thisIndex = self.indexOfElements[index.hashValue] {
-            return self.arrayOfElements[thisIndex]
-        } else {
-            return nil
+        return queue.dispatch_sync() {
+            if let thisIndex = self.indexOfElements[index.hashValue] {
+                return self.arrayOfElements[thisIndex]
+            } else {
+                return nil
+            }
         }
     }
     
@@ -59,46 +72,56 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
         // keep the index of the next element in the iteration
         var nextIndex = 0
         
-        return AnyGenerator<Element> {
-            var returnValue: Element?
-            if (nextIndex < self.arrayOfElements.count) {
-                returnValue = self.arrayOfElements[nextIndex]
-                nextIndex += 1
+        return queue.dispatch_sync() {
+            return AnyGenerator<Element> {
+                var returnValue: Element?
+                if (nextIndex < self.arrayOfElements.count) {
+                    returnValue = self.arrayOfElements[nextIndex]
+                    nextIndex += 1
+                }
+                return returnValue
             }
-            return returnValue
         }
     }
 
     // MARK: Add, modify, and delete methods
     
     func insert(element: Element) {
-        if let thisIndex = self.indexOfElements[element.hashValue] {
-            self.arrayOfElements[thisIndex] = element
-        } else {
-            self.indexOfElements[element.hashValue] = self.count
-            self.arrayOfElements.append(element)
+        queue.dispatch_asyncbarrier() {
+            if let thisIndex = self.indexOfElements[element.hashValue] {
+                self.arrayOfElements[thisIndex] = element
+            } else {
+                self.indexOfElements[element.hashValue] = self.count
+                self.arrayOfElements.append(element)
+            }
         }
     }
     
     func remove(element: Element) {
-        if let thisIndex = self.indexOfElements[element.hashValue] {
-            self.arrayOfElements.removeAtIndex(thisIndex)
-            self.indexOfElements[element.hashValue] = nil
-            self.buildDictionary()
+        queue.dispatch_asyncbarrier() {
+            if let thisIndex = self.indexOfElements[element.hashValue] {
+                self.arrayOfElements.removeAtIndex(thisIndex)
+                self.indexOfElements[element.hashValue] = nil
+                self.buildDictionary()
+            }
         }
     }
     
     func removeAtIndex<H: Hashable>(index: H) {
-        if let thisIndex = self.indexOfElements[index.hashValue] {
-            self.arrayOfElements.removeAtIndex(thisIndex)
-            self.indexOfElements[index.hashValue] = nil
-            self.buildDictionary()
+        queue.dispatch_asyncbarrier() {
+            if let thisIndex = self.indexOfElements[index.hashValue] {
+                self.arrayOfElements.removeAtIndex(thisIndex)
+                self.indexOfElements[index.hashValue] = nil
+                self.buildDictionary()
+            }
         }
     }
     
     func removeAll() {
-        self.arrayOfElements.removeAll()
-        self.indexOfElements.removeAll()
+        queue.dispatch_asyncbarrier() {
+            self.arrayOfElements.removeAll()
+            self.indexOfElements.removeAll()
+        }
     }
     
     private func buildDictionary() {
@@ -116,36 +139,44 @@ class IndexedArray<Element: Hashable>: NSObject, SequenceType {
     
     func sort(handler: (first: Element, second: Element) -> Bool) -> Self {
         let returnArray = self.dynamicType.init()
-        returnArray.arrayOfElements = self.arrayOfElements.sort(handler)
+        dispatch_sync(queue) {
+            returnArray.arrayOfElements = self.arrayOfElements.sort(handler)
+        }
         returnArray.buildDictionary()
         return returnArray
     }
     
     func filter(handler: (element: Element) -> Bool) -> Self {
         let returnArray = self.dynamicType.init()
-        returnArray.arrayOfElements = self.arrayOfElements.filter(handler)
+        dispatch_sync(queue) {
+            returnArray.arrayOfElements = self.arrayOfElements.filter(handler)
+        }
         returnArray.buildDictionary()
         return returnArray
     }
     
     func map<T>(transform: Element throws -> T) rethrows -> [T] {
-        return try self.arrayOfElements.map(transform)
+        return dispatch_sync(queue) {
+            try self.arrayOfElements.map(transform)
+        }
     }
     
     func reduce<T>(initial: T?, combine: (T?, Element) -> T?) -> T? {
-        var returnVal: T?
-        returnVal = self.arrayOfElements.reduce(initial, combine: combine)
-        return returnVal
+        return dispatch_sync(queue) {
+            return self.arrayOfElements.reduce(initial, combine: combine)
+        }
     }
     
     func reduce<T>(initial: T, combine: (T, Element) throws -> T) rethrows -> T {
-        var returnVal: T!
-        returnVal = try self.arrayOfElements.reduce(initial, combine: combine)
-        return returnVal
+        return dispatch_sync(queue) {
+            return try self.arrayOfElements.reduce(initial, combine: combine)
+        }
     }
     
     var asArray: Array<Element> {
-        return arrayOfElements
+        return dispatch_sync(queue) {
+            return arrayOfElements
+        }
     }
     
 }
